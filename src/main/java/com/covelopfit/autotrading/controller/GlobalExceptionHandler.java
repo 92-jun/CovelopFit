50bf994715ab418008162a233b4141f72f021d49
goooood-D <gooddebugger@kakao.com>
2021-11-21 12:27:25 +0900

    [ISSUE4] responseentity 적용

--- src/main/java/com/covelopfit/autotrading/controller/GlobalExceptionHandler.java
+++ src/main/java/com/covelopfit/autotrading/controller/GlobalExceptionHandler.java
@@ -1,9 +1,9 @@
 package com.covelopfit.autotrading.controller;
 
-import com.covelopfit.autotrading.response.AutoTradingCommonResponse;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -15,10 +15,10 @@ public class GlobalExceptionHandler {
 
     @ResponseBody
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    @ExceptionHandler(value = {RuntimeException.class})
-    protected AutoTradingCommonResponse exceptionHandler(Exception e) {
+    @ExceptionHandler(value = {Exception.class})
+    protected ResponseEntity exceptionHandler(Exception e) {
         log.error(String.format("[error] 확인되지 않은 에러 %s", e.toString()));
-        return new AutoTradingCommonResponse(HttpStatus.INTERNAL_SERVER_ERROR, "서버 에러", null);
+        return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
 }
--- src/main/java/com/covelopfit/autotrading/controller/OrderController.java
+++ src/main/java/com/covelopfit/autotrading/controller/OrderController.java
@@ -2,21 +2,16 @@ package com.covelopfit.autotrading.controller;
 
 import com.covelopfit.autotrading.dto.OrderApiResponse;
 import com.covelopfit.autotrading.dto.OrderForm;
-import com.covelopfit.autotrading.response.AutoTradingCommonResponse;
 import com.covelopfit.autotrading.service.OrderService;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.HashMap;
-import java.util.Map;
 
 @Slf4j
 @Controller
@@ -32,14 +27,14 @@ public class OrderController {
 
     @PostMapping(value = "order")
     @ResponseBody
-    public AutoTradingCommonResponse postOrder(@ModelAttribute @Valid OrderForm orderForm) {
+    public ResponseEntity postOrder(@ModelAttribute @Valid OrderForm orderForm) {
         log.debug(orderForm.toString());
         OrderApiResponse orderApiResponse = orderService.postOrder(orderForm);
 
         if(orderApiResponse == null){
-            return new AutoTradingCommonResponse(HttpStatus.INTERNAL_SERVER_ERROR, "error", null);
+            return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
         }
 
-        return new AutoTradingCommonResponse(HttpStatus.OK, "success", orderApiResponse);
+        return new ResponseEntity(orderApiResponse, HttpStatus.OK);
     }
 }
